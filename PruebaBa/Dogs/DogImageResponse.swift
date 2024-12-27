a93b23e243d0ee4515922dfefc9f710a759ab201
“Pedro” <tostado_mt@hotmail.com>
2024-12-26 19:40:19 -0600

    pruebaV1

---
+++ PruebaBa/Dogs/DogImageResponse.swift
@@ -0,0 +1,13 @@
+//
+//  DogImageResponse.swift
+//  PruebaBa
+//
+//  Created by PEDRO MENDEZ on 26/12/24.
+//
+
+import Foundation
+
+struct DogImageResponse: Decodable {
+    let message: String
+    let status: String
+}
---
+++ PruebaBa/Login/LoginError.swift
@@ -0,0 +1,31 @@
+//
+//  Untitled.swift
+//  PruebaBa
+//
+//  Created by PEDRO MENDEZ on 26/12/24.
+//
+
+import Foundation
+
+enum LoginError: LocalizedError {
+    case emptyData
+    case invalidCredentials
+    case serverError
+    case parsingError
+    case errorRetrievingInformation
+
+    var errorDescription: String? {
+        switch self {
+        case .emptyData:
+            return "Los campos están vacíos."
+        case .invalidCredentials:
+            return "Credenciales inválidas."
+        case .serverError:
+            return "Error del servidor. Intenta más tarde."
+        case .parsingError:
+            return "Error al procesar la información."
+        case .errorRetrievingInformation:
+            return "No se puede recuperar la información."
+        }
+    }
+}
